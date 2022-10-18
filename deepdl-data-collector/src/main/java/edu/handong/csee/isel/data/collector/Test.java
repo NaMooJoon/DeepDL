004efecf0769ddd347ca508ece3f765cf300e01a
unknown <hahyun8587@gmail.com>
2022-10-19 06:22:34 +0900

    feat: get splitting commit

--- deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/Test.java
+++ deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/Test.java
@@ -1,15 +1,20 @@
 package edu.handong.csee.isel.data.collector;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 
-import edu.handong.csee.isel.data.collector.io.CommitHashReader;
-import edu.handong.csee.isel.data.collector.io.PropertyWriter;
+//import edu.handong.csee.isel.data.collector.io.CommitHashReader;
+//import edu.handong.csee.isel.data.collector.io.PropertyWriter;
+import edu.handong.csee.isel.data.collector.github.GitHubSearcher;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
 
 public class Test {
     public static void main(String[] args) {
         try {
+            /*
             String[] projects = { "juddi" };
             ArrayList<String>[] hashes = new ArrayList[1];
 
@@ -17,10 +22,15 @@ public class Test {
                     .readCommitHashes(projects[0], "2004-01-01", "2022-12-31");
     
             new PropertyWriter("test.json").writePySZZJson(projects, hashes);
-
+            */
+            System.out.println(new GitHubSearcher("C:/activemq").getSplittingCommit().getRawBuffer());
         } catch(FileNotFoundException e) {
             e.printStackTrace();
         } catch(IOException e) {
+            e.printStackTrace();
+        } catch(NoHeadException e) {
+            e.printStackTrace();
+        } catch(GitAPIException e) {
             e.printStackTrace();
         }
     }
