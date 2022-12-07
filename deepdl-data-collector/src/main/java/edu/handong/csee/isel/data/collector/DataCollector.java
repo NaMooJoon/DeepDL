a6e747287036756f65a9c4a73154a9d344343674
unknown <hahyun8587@gmail.com>
2022-12-07 21:58:44 +0900

    feat: support single project data collection

--- deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/DataCollector.java
+++ deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/DataCollector.java
@@ -13,15 +13,16 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import edu.handong.csee.isel.data.collector.core.Extractor;
 import edu.handong.csee.isel.data.collector.core.GitHubSearcher;
 import edu.handong.csee.isel.data.collector.util.Resources;
+import edu.handong.csee.isel.data.collector.util.Utils;
 
 /**
  * Collector that collects data for DeepDL model.
  */
 public class DataCollector {
     private String fileSeparator = System.getProperty("file.separator");
-    private String projectPath = getProjectPath();
+    private String projectPath = Utils.getProjectPath();
     private GitHubSearcher searcher = new GitHubSearcher();
-    private Extractor extractor = new Extractor(projectPath);
+    private Extractor extractor = new Extractor();
 
     /**
      * Collects data for DeepDL model.
@@ -30,76 +31,59 @@ public class DataCollector {
         final String END_DATE = "2021-11-30";
         final float TRAIN_RATIO = 0.6F;
 
-        String repoPath = String.join(fileSeparator, 
-                                      projectPath, "out", "repositories");
         String snapshotPath = String.join(fileSeparator,  
-                                          projectPath, "out", "snapshots");   
+                                          projectPath, "out", "snapshot");   
         List<String>[] resources = new List[4];
         RevCommit[] splittingCommits;
-        String[] startDates;
         int numRepositories;
 
         loadResources(resources);
 
         numRepositories = resources[Resources.URL.ordinal()].size();
         splittingCommits = new RevCommit[numRepositories];
-        startDates = new String[numRepositories];
 
         try {
+            Files.createDirectory(Path.of(projectPath, "out", "bfc"));
+            Files.createDirectory(Path.of(projectPath, "out", "bic"));
+
             for (int i = 0; i < numRepositories; i++) {
-                searcher.cloneRepository(
-                        resources[Resources.URL.ordinal()].get(i), 
-                        String.join(fileSeparator, repoPath, 
-                                resources[Resources.REPOUSER.ordinal()]
-                                .get(i)));
-                searcher.cloneRepository(
-                        resources[Resources.URL.ordinal()].get(i), 
-                        snapshotPath);
-                searcher.changeRepository(
+                String startDate;
+                String repoPath = 
                         String.join(fileSeparator, snapshotPath, 
+                                resources[Resources.REPOUSER.ordinal()].get(i),
                                 resources[Resources.REPOSITORY.ordinal()]
-                                .get(i), ".git"));
+                                .get(i));
+                                
+                searcher.cloneRepository(
+                        resources[Resources.URL.ordinal()].get(i), repoPath);
+                searcher.changeRepository(
+                        String.join(fileSeparator, repoPath, ".git"));
 
                 splittingCommits[i] = 
                         searcher.getSplittingCommit(TRAIN_RATIO, 
                                                     null, END_DATE);
-                
-                searcher.checkoutToSnapshot(splittingCommits[i]);
-
-                startDates[i] = 
+                startDate = 
                         new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(
                                 Date.from(splittingCommits[i]
                                           .getAuthorIdent()
                                           .getWhen()
                                           .toInstant()
                                           .plusSeconds(1L)));
+                extractor.extractBFC(
+                        resources[Resources.URL.ordinal()].get(i), 
+                        resources[Resources.KEY.ordinal()].get(i),
+                        resources[Resources.REPOUSER.ordinal()].get(i), 
+                        resources[Resources.REPOSITORY.ordinal()].get(i),
+                        startDate, END_DATE);
+                //extractor.extractBIC(
+                //        resources[Resources.REPOSITORY.ordinal()].get(i));
+                
+                searcher.checkoutToSnapshot(splittingCommits[i]);
             }
-            extractor.extractBFC(resources, startDates, END_DATE);
-            extractor.extractBIC();   
 
-
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Gets this project's absolute root path.
-     * @return the project path
-     */
-    private String getProjectPath() {
-        final int NUM_STEPS = 3; 
-        
-        String regex = fileSeparator.equals("\\") 
-                ? fileSeparator + fileSeparator 
-                : fileSeparator;
-        String[] splittedCwd = System.getProperty("user.dir").split(regex);        
-        String[] splittedProjectPath = new String[splittedCwd.length - NUM_STEPS];  
-        
-        for (int i = 0; i < splittedProjectPath.length; i++) {
-            splittedProjectPath[i] = splittedCwd[i];
-        }  
-        return String.join(fileSeparator, splittedProjectPath);  
     }
 
     /**
@@ -114,18 +98,20 @@ public class DataCollector {
         Path resourcePath = Path.of(projectPath, "src", "main", "resources");
 
         try {
-            resources[0] = Files.readAllLines(resourcePath.resolve("url"));
-            resources[1] = Files.readAllLines(resourcePath.resolve("jira-key"));
+            resources[Resources.URL.ordinal()] 
+                    = Files.readAllLines(resourcePath.resolve("url"));
+            resources[Resources.KEY.ordinal()] 
+                    = Files.readAllLines(resourcePath.resolve("jira-key"));
             
             for (int i = 2; i <= 3; i++) {
                 resources[i] = new ArrayList<String>();
             }
 
-            for (String url : resources[0]) {
+            for (String url : resources[Resources.URL.ordinal()]) {
                 String[] splitted = url.split("/");
                 
                 for (int i = 2; i <= 3; i++) {
-                    resources[i].add(splitted[i - 1]);
+                    resources[i].add(splitted[i + 1]);
                 }
             }
         } catch(IOException e) {
