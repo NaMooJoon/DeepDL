baa191d2f94a3753944a23c553a998730905bfdf
unknown <hahyun8587@gmail.com>
2022-12-07 21:55:00 +0900

    feat: collect data from the projects

---
+++ deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/Main.java
@@ -0,0 +1,9 @@
+package edu.handong.csee.isel.data.collector;
+
+import edu.handong.csee.isel.data.collector.DataCollector;
+
+public class Main {
+    public static void main(String[] args) {
+        new DataCollector().collect();
+    }
+}
