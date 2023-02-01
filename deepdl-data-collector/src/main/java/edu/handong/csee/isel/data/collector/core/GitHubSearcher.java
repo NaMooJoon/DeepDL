cd392fea1727cc637afc34f34697b46ebd29df30
Hahyun Baek <hahyun8587@gmail.com>
2023-02-01 13:26:52 +0900

    feat: implement convertHashToCommit and convertHashToPreviousCommit

--- deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/core/GitHubSearcher.java
+++ deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/core/GitHubSearcher.java
@@ -1,5 +1,6 @@
 package edu.handong.csee.isel.data.collector.core;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
@@ -7,29 +8,76 @@ import java.util.ArrayList;
 
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 
 /**
- * Class that gets essential ingredients for DeepDL data collection from GitHub. 
+ * Class that gets data from GitHub. 
  */
 public class GitHubSearcher {
     private Git git;
-    
-    public GitHubSearcher() {}
 
     /**
-     * Creates <code>Git</code> object for using git commands. 
-     * @param gitDir a local git repository metadata directory 
-     * @throws IOException
+     * Clones the repository to the given directory.
+     * Creates the directory by creating all of the nonexistent parent directory if there is no given directory.
+     * @param uri GitHub respository uri
+     * @param dir the directory
+     * @throws InvaildRemoteException
+     * @throws TransportException
+     * @throws GitAPIException
      */
-    public GitHubSearcher(String gitDir) throws IOException {
-        git = Git.open(new File(gitDir));
+    public void cloneRepository(String uri, String dir) 
+            throws InvalidRemoteException, 
+                    TransportException, GitAPIException {
+        File directory = new File(dir);
+        CloneCommand cloneCommand = new CloneCommand();
+
+        cloneCommand.setURI(uri).setDirectory(directory).call().close();
     }
 
+    /**
+     * Gets diff between the given two commits.
+     * The given commits should be in this instance's object database.
+     * @param oldCommit the old commit
+     * @param newCommit the new commit
+     * @return diff context between the <code>oldCommit</code> and <code>newCommit</code>
+     * @throws GitAPIException
+     * @throws IncorrectObjectTypeException
+     * @throws MissingObjectException
+     * @throws IOException
+     */
+    public String[] diffCommits(RevCommit oldCommit, RevCommit newCommit) 
+            throws GitAPIException, IncorrectObjectTypeException, 
+                   MissingObjectException, IOException { 
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectReader or = git.getRepository().newObjectReader();
+        RevWalk revWalk = new RevWalk(or);
+
+        git.diff()
+           .setOutputStream(out)
+           .setOldTree(new CanonicalTreeParser(
+                    null, 
+                    or, 
+                    oldCommit.getTree()))
+           .setNewTree(new CanonicalTreeParser( 
+                    null, 
+                    or,
+                    newCommit.getTree()))
+          .call();
+        or.close();
+        revWalk.close();
+       
+        return out.toString().split("\n");
+    }   
+     
     /**
      * Gets splitting commit from this instance's repository.
      * The splitting commit splits this instance's repository's commits from [<code>startDate</code>, <code>endDate</code>) in ratio of <code>trainRatio</code> : 1 - <code>trainRatio</code>.
@@ -38,6 +86,9 @@ public class GitHubSearcher {
      * @param startDate start date of the repository's commit - yyyy-[M]M-[d]d
      * @param endDate end date of the repository's commit - yyyy-[M]M-[d]d
      * @return the splitting commit
+     * @throws GitAPIException
+     * @throws NoHeadException
+     * @throws IOException
      */
     public RevCommit getSplittingCommit(float trainRatio, 
             String startDate, String endDate) 
@@ -76,22 +127,44 @@ public class GitHubSearcher {
            .call();
     }
 
-     /**
-     * Clones the repository to the given directory.
-     * Creates the directory by creating all of the nonexistent parent directory if there is no given directory.
-     * @param uri GitHub respository uri
-     * @param dir the directory
-     * @throws InvaildRemoteException
-     * @throws TransportException
-     * @throws GitAPIException
+    /**
+     * Converts the given commit hash into the <code>RevCommit</code> instance.
+     * The given commit hash should be in this instance's object database.
+     * @param hash the commit hash
+     * @return the <code>RevCommit</code> instance 
+     * @throws MissingObjectException
+     * @throws IncorrectObjectTpeException
+     * @throws IOException
      */
-    public void cloneRepository(String uri, String dir) 
-            throws InvalidRemoteException, 
-                    TransportException, GitAPIException {
-        File directory = new File(dir);
-        CloneCommand cloneCommand = new CloneCommand();
+    public RevCommit convertHashToCommit(String hash) 
+            throws MissingObjectException, IncorrectObjectTypeException, 
+                   IOException {
+        RevWalk revWalk = new RevWalk(git.getRepository());
+        RevCommit commit = revWalk.parseCommit(RevCommit.fromString(hash));        
+        revWalk.close();
 
-        cloneCommand.setURI(uri).setDirectory(directory).call().close();
+        return commit;
+    }
+
+    /**
+     * Converts the given commit hash into the previous <code>RevCommit</code> instance.
+     * The given commit hash should be in this instance's object database.
+     * @param hash the commit hash
+     * @return the previous <code>RevCommit</code> instance
+     * @throws MissingObjectException
+     * @throws IncorrectObjectTypeException
+     * @throws IOExcpetion
+     */
+    public RevCommit convertHashToPreviousCommit(String hash) 
+            throws MissingObjectException, IncorrectObjectTypeException, 
+                   IOException {
+        RevWalk revWalk = new RevWalk(git.getRepository());
+        RevCommit commit = revWalk.parseCommit(RevCommit.fromString(hash))
+                                  .getParent(0);        
+        revWalk.parseCommit(commit.getId());
+        revWalk.close();
+
+        return commit;
     }
 
     /**
