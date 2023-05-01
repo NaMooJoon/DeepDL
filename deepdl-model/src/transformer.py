7da0919feec213a67126a454bf4a8b260f23f0b1
Hahyun Baek <hahyun8587@gmail.com>
2023-05-02 01:40:54 +0900

    fix: fix type error in update_state of TransformerAccuracy

--- deepdl-model/src/transformer.py
+++ deepdl-model/src/transformer.py
@@ -51,8 +51,7 @@ class EncoderLayer(tf.keras.layers.Layer):
     self.dropout2 = tf.keras.layers.Dropout(rate)
 
   def call(self, x, training, mask):
-    attn_out = self.mha(x, x, x, 
-                        mask, 
+    attn_out = self.mha(x, x, x, mask, 
                         return_attention_scores=False, 
                         training=training, 
                         use_causal_mask=False)  # (batch_size, input_seq_len, d_model)
@@ -86,19 +85,19 @@ class DecoderLayer(tf.keras.layers.Layer):
     self.dropout2 = tf.keras.layers.Dropout(rate)
     self.dropout3 = tf.keras.layers.Dropout(rate)
 
-  def call(self, x, enc_output, training, padding_mask):  # modified: remove param look_ahead_mask
+  def call(self, x, enc_output, training, mask):  # modified: remove param look_ahead_mask
     # enc_output.shape == (batch_size, input_seq_len, d_model)
 
-    attn1, attn_weights_block1 = self.mha1(x, x, x,                             # modified: calling tf.keras.MultiHeadAttention
-                                           padding_mask, 
+    # modified: calling tf.keras.MultiHeadAttention
+    attn1, attn_weights_block1 = self.mha1(x, x, x, mask                            
                                            return_attention_scores=True, 
                                            training=training, 
                                            use_causal_mask=True)  # (batch_size, target_seq_len, d_model) 
     attn1 = self.dropout1(attn1, training=training)
     out1 = self.layernorm1(attn1 + x)
 
-    attn2, attn_weights_block2 = self.mha2(out1, enc_output, enc_output,         # modified: calling tf.keras.MultiHeadAttention
-                                           padding_mask, 
+    # modified: calling tf.keras.MultiHeadAttention
+    attn2, attn_weights_block2 = self.mha2(out1, enc_output, enc_output, mask, 
                                            return_attention_scores=True, 
                                            training=training, 
                                            use_causal_mask=False)  # (batch_size, target_seq_len, d_model)
@@ -163,7 +162,7 @@ class Decoder(tf.keras.layers.Layer):
                        for _ in range(num_layers)]
     self.dropout = tf.keras.layers.Dropout(rate)
 
-  def call(self, x, enc_output, training, padding_mask): # modified: remove param look_ahead_mask
+  def call(self, x, enc_output, training, mask): # modified: remove param look_ahead_mask
     seq_len = tf.shape(x)[1]
     attention_weights = {}
 
@@ -174,14 +173,14 @@ class Decoder(tf.keras.layers.Layer):
     x = self.dropout(x, training=training)
 
     for i in range(self.num_layers):
-      x, block1, block2 = self.dec_layers[i](x, enc_output, 
-                                             training, padding_mask) # modified: remove arg look_ahead_mask
+      x, block1, block2 = self.dec_layers[i](x, enc_output, training, mask) # modified: remove arg look_ahead_mask
 
       attention_weights[f'decoder_layer{i+1}_block1'] = block1
       attention_weights[f'decoder_layer{i+1}_block2'] = block2
 
     # x.shape == (batch_size, target_seq_len, d_model)
     return x, attention_weights 
+  
   
 class TransformerAccuracy(tf.keras.metrics.Metric):
   
@@ -193,8 +192,8 @@ class TransformerAccuracy(tf.keras.metrics.Metric):
     self.total = self.add_weight('total', 
                                  dtype=tf.float32, initializer='zeros')
     
-  def update_state(self, y_true, y_pred):
-    accuracies = tf.equal(tf.argmax(pred, axis=2), y_true)
+  def update_state(self, y_true, y_pred, **kwargs):
+    accuracies = tf.equal(tf.argmax(y_pred, axis=2), y_true)
     mask = tf.math.logical_not(tf.math.equal(y_true, 0))
     
     accuracies = tf.cast(tf.math.logical_and(accuracies, mask), 
