21d2cf1deab8184257260b1fb74438849dcf1c8c
Hahyun Baek <hahyun8587@gmail.com>
2023-05-04 15:05:32 +0900

    refactor: use cli

--- deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/Main.java
+++ deepdl-data-collector/src/main/java/edu/handong/csee/isel/data/collector/Main.java
@@ -3,7 +3,7 @@ package edu.handong.csee.isel.data.collector;
 public class Main {
     public static void main(String[] args) {
         try {
-            new DataCollector().collect(args[1], args[2]);
+            new DataCollector().collect(args[0], args[1]);
         } catch (Exception e) {
             e.printStackTrace();
         }
